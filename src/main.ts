9979432d54cdb43bd20875d7915c006d523b5d4c
Cesar <88-8@libe.com.mx>
2024-10-04 12:01:59 -0600

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { BASE_PATH } from './api/generated';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
+    { provide: BASE_PATH, useValue: environment.apiUrl },
+    provideNoopAnimations(),
   ],
 });
